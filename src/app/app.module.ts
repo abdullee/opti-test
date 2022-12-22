276d5ddd62ae05046d2ef2da1d4a32f3e4bec217
abdulhakeem1998 <58566210+abdullee@users.noreply.github.com>
2022-12-22 21:35:17 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule,Title  } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgwWowModule } from 'ngx-wow';
+import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { CafmComponent } from './cafm/cafm.component';
+import { AdvantageComponent } from './advantage/advantage.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServicesComponent,
+    CafmComponent,
+    AdvantageComponent,
+    TeamComponent,
+    ContactComponent,
+    HomeComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,    
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+    providers: [
+    Title
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
